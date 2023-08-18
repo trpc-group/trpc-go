07f2d03c740a81dcf70914759bfb39379dd61ea2
cooperyan <>
2023-08-18 17:05:41 +0800

    make hello world example as simple as possible.

---
+++ examples/helloworld/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+
+	"trpc.group/trpc-go/trpc-go"
+	"trpc.group/trpc-go/trpc-go/examples/helloworld/pb"
+	"trpc.group/trpc-go/trpc-go/log"
+)
+
+func main() {
+	s := trpc.NewServer()
+	pb.RegisterGreeterService(s, &Greeter{})
+	if err := s.Serve(); err != nil {
+		log.Error(err)
+	}
+}
+
+type Greeter struct{}
+
+func (g Greeter) Hello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Infof("got hello request: %s", req.Msg)
+	return &pb.HelloReply{Msg: "Hello " + req.Msg + "!"}, nil
+}
