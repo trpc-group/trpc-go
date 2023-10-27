a76e72ad72eb874a05ffd7f5be4c3d19d588c8ac
cyhone <cyhone@qq.com>
2023-10-27 16:42:34 +0800

    wrap error msg
    Co-authored-by: wineandchord <guoqizhou123123@qq.com>

--- http/transport.go
+++ http/transport.go
@@ -201,7 +201,7 @@ func (t *ServerTransport) getListener(network, addr string) (net.Listener, error
 		}
 		ln, ok = pln.(net.Listener)
 		if !ok {
-			return nil, errors.New("invalid net.Listener")
+			return nil, fmt.Errorf("invalid listener type, want net.Listener, got %T", pln)
 		}
 		return ln, nil
 	}
