a4d5e426f19bb9e08a6109f1b4e82132e4dcbd4c
junevanlong <junevanlong@tencent.com>
2026-06-15 19:40:56 +0800

    transport: add keeporder license headers

--- internal/keeporder/client.go
+++ internal/keeporder/client.go
@@ -1,3 +1,16 @@
+//
+//
+// Tencent is pleased to support the open source community by making tRPC available.
+//
+// Copyright (C) 2023 Tencent.
+// All rights reserved.
+//
+// If you have downloaded a copy of the tRPC source code from Tencent,
+// please note that tRPC source code is licensed under the  Apache 2.0 License,
+// A copy of the Apache 2.0 License is included in this file.
+//
+//
+
 // Package keeporder provides keep order functionalities.
 package keeporder
 
